48a5a8fa3691fc61a3621baf60af85e73612d38b
kxy_c <my369762419@vip.qq.com>
2017-08-28 11:39:36 +0800

    modify

--- 04_RM_Duplicates_and_depth_summary.sh
+++ 04_RM_Duplicates_and_depth_summary.sh
@@ -6,15 +6,15 @@ cd ${id%%_*}
 echo ${id%%_*}
 echo "start  04_1_rm_duplicates_and_statistics.sh"
 
-/home/kxyu/codes/WES_pipeline/04_1_rm_duplicates_and_statistics.sh
+/home/kxyu/codes/WES_pipeLine/04_1_rm_duplicates_and_statistics.sh
 
 echo "start  04_2_chromosome_depth_summary.py"
 
-python3 /home/kxyu/codes/WES_pipeline/04_2_chromosome_depth_summary.py  ${id%%_*}_depth.txt ${id%%_*}_chr_depth_summary.txt
+python3 /home/kxyu/codes/WES_pipeLine/04_2_chromosome_depth_summary.py  ${id%%_*}_depth.txt ${id%%_*}_chr_depth_summary.txt
 
 echo "make plot"
 path= $(cd `dirname $0`; pwd)
-Rscript /home/kxyu/codes/WES_pipeline/04_3_make_depth_summary_plot.R  $path  /${id%%_*}_chr_depth_summary.txt
+Rscript /home/kxyu/codes/WES_pipeLine/04_3_make_depth_summary_plot.R  $path  /${id%%_*}_chr_depth_summary.txt
 
 cd ..
 
